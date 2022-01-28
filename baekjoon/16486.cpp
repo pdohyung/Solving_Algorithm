33fde973d696faea55ff3a965586b987ca9e31ff
Dohyung Kim <pdohyung@gmail.com>
2022-01-28 22:09:23 +0900

    no message

---
+++ baekjoon/16486.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double d1, d2;
+    double PI = 3.141592;
+    cin >> d1 >> d2;
+    cout << fixed;
+    cout.precision(6);
+    cout << d1*2 + d2*2*PI;
+    return 0;
+}
