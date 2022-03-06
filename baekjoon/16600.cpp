81b3015cad6fc88dfe1c6805abab3a30a2742f84
admin <pdohyung@gmail.com>
2022-03-06 23:33:06 +0900

    no message

---
+++ baekjoon/16600.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double a;
+    cin >> a;
+    a = sqrt(a);
+    cout << fixed;
+    cout.precision(8);
+    cout << a*4;
+    return 0;
+}
