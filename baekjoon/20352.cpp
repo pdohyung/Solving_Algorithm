fcfa1a6cba6d7d1f29f697fa4fe99cb3eeb2750d
admin <pdohyung@gmail.com>
2022-03-14 00:10:45 +0900

    no message

---
+++ baekjoon/20352.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void) {
+    double pi = 3.1415926535;
+    double a;
+    cin >> a;
+    double res = sqrt(a / pi) * 2 * pi;
+    printf("%.7lf\n", res);
+    return 0;
+    }
