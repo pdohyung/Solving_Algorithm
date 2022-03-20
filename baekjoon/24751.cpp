bec9ad81760f9fa26c7f887f43c6eaa4c5072a7c
admin <pdohyung@gmail.com>
2022-03-21 01:47:09 +0900

    no message

---
+++ baekjoon/24751.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double n;
+    cin >> n;
+    cout << fixed;
+    cout << setprecision(4);
+    cout << 100 / n << '\n' << 100 / (100-n);
+    return 0;
+}
