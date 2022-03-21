01fc6685196afc5102714697f4581cdcc39788a0
admin <pdohyung@gmail.com>
2022-03-22 02:50:36 +0900

    no message

---
+++ baekjoon/20353.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double n;
+    cin >> n;
+
+    double res = sqrt(n) * 4;
+    cout << fixed << setprecision(7);
+    cout << res;
+    return 0;
+}
